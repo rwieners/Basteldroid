1b57c395b4d8bcf9c1f3c3298d4183a1392bdc97
rwieners <rene.wieners@gmail.com>
2018-09-13 11:41:45 +0200

    Lektion 3 komplett

--- src/basteldroid/StartingClass.java
+++ src/basteldroid/StartingClass.java
@@ -4,8 +4,10 @@ package basteldroid;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class StartingClass extends Applet implements Runnable {
+public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	@Override
 	public void init() {
@@ -13,7 +15,7 @@ public class StartingClass extends Applet implements Runnable {
 		setSize(800, 400);
 		setBackground(Color.BLACK);
 		setFocusable(true);
-
+		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Q-Bot Alpha");
 	}
@@ -46,6 +48,59 @@ public class StartingClass extends Applet implements Runnable {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		switch(e.getKeyCode()){
+		case KeyEvent.VK_UP:
+			System.out.println("Bewege dich hoch");
+			break;
+		case KeyEvent.VK_DOWN:
+			System.out.println("Bewege dich runter");
+			break;
+		case KeyEvent.VK_LEFT:
+			System.out.println("Bewege dich links");
+			break;
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Bewege dich rechts");
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println("Spring");
+			break;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		switch(e.getKeyCode()){
+		case KeyEvent.VK_UP:
+			System.out.println("Stop dich zu bewegen");
+			break;
+		case KeyEvent.VK_DOWN:
+			System.out.println("Stop dich zu bewegen");
+			break;
+		case KeyEvent.VK_LEFT:
+			System.out.println("Stop dich zu bewegen");
+			break;
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Stop dich zu bewegen");
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println("Stop zu springen");
+			break;
 		}
 	}
 
